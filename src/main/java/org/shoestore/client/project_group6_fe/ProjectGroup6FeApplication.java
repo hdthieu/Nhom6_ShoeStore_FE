16a35414a50a47c2f40fb97ae0ca2812e73669ce
Hồ Đức Trí Hiếu <hoductrihieu@gmail.com>
2025-03-08 13:55:43 +0700

    initialized

---
+++ src/main/java/org/shoestore/client/project_group6_fe/ProjectGroup6FeApplication.java
@@ -0,0 +1,13 @@
+package org.shoestore.client.project_group6_fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGroup6FeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGroup6FeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shoestore/client/project_group6_fe/ProjectGroup6FeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shoestore.client.project_group6_fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGroup6FeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
